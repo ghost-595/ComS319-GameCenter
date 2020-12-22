07e96759d324ea3f64d7348ce475604bc055e40f
ghost-595 <jkelly5@iastate.edu>
2020-12-22 08:17:39 -0600

    removed files

--- JonKelly-Tetris/src/components/StartButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { StyledStartButton } from './styles/StyleStartButton';
-
-const StartButton = ({ callback }) => (
-    <StyledStartButton onClick={callback}>Start Game</StyledStartButton>
-)
-
-export default StartButton;
